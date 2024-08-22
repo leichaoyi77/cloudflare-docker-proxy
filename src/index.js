74ac6c9dff4366d0e0841f15f2eb7339e46ab712
leichaoyi77 <101256684+leichaoyi77@users.noreply.github.com>
2024-08-22 22:56:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.leichaoyi.top": dockerHub,
+  "quay.leichaoyi.top": "https://quay.io",
+  "gcr.leichaoyi.top": "https://gcr.io",
+  "k8s-gcr.leichaoyi.top": "https://k8s.gcr.io",
+  "k8s.leichaoyi.top": "https://registry.k8s.io",
+  "ghcr.leichaoyi.top": "https://ghcr.io",
+  "cloudsmith.leichaoyi.top": "https://docker.cloudsmith.io",
+  "ecr.leichaoyi.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.leichaoyi.top": dockerHub,
 };
 
 function routeByHosts(host) {
